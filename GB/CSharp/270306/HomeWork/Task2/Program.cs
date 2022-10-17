40d80df6610117f3835441a059fe6a0995926d0c
Johnny Catsvill <jc0365@yandex.ru>
2022-10-17 16:31:38 +0300

    Lesson 270306. Home work. Task2

---
+++ GB/CSharp/270306/HomeWork/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a, b, max;
+
+Console.WriteLine("Введите 1-е число:");
+a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 2-е число:");
+b = int.Parse(Console.ReadLine()!);
+
+max = a > b? a : b;
+Console.WriteLine($"Максимальное число: {max}");
