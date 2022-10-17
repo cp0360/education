2796951a4c307c58000aaa246b45d051e60defa3
Johnny Catsvill <jc0365@yandex.ru>
2022-10-17 16:31:50 +0300

    Lesson 270306. Home work. Task4

---
+++ GB/CSharp/270306/HomeWork/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a, b, c, max;
+
+Console.WriteLine("Введите 1-е число:");
+a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 2-е число:");
+b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 3-е число:");
+c = int.Parse(Console.ReadLine()!);
+
+max = a;
+
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine($"Максимальное число: {max}");
