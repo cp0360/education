d4679777e53dd59a71098947c922caecadc1252f
Johnny Catsvill <jc0365@yandex.ru>
2022-10-18 14:40:02 +0300

    Lesson 270307. Task14.

---
+++ GB/CSharp/270307/Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 14.
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine()!);
+String otvet;
+
+if (x % 7 == 0 && x % 23 == 0)
+    otvet = "Да";
+else
+    otvet = "Нет";
+
+Console.WriteLine($"число {x} кратно 7 и 23 -> {otvet}");
