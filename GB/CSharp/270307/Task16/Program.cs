991f556e3432f8e7c7beb560a4c2cba5af2d01b7
Johnny Catsvill <jc0365@yandex.ru>
2022-10-18 14:40:16 +0300

    Lesson 270307. Task16.

---
+++ GB/CSharp/270307/Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 16
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите 1-е число:");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 1-е число:");
+int b = int.Parse(Console.ReadLine()!);
+
+String otvet = a * a == b || b * b == a ? "Да" : "Нет";
+
+Console.WriteLine($"Является ли одно число квадратом другого? {otvet}.");
