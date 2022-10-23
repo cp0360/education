7c51714dd72eeb9c4f3debefdd91091de43ce930
Senior Cat <sc0372q@gmail.com>
2022-10-23 20:05:47 +0300

    Lesson 270308. Task 21

---
+++ GB/CSharp/270308/Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+
+using System;
+
+
+double GetDistance(double x1, double y1, double x2, double y2)
+{
+    double X = Math.Pow(x1-x2, 2);
+    double Y = Math.Pow(y1-y2, 2);
+    double distance = Math.Sqrt(X + Y);
+    return distance;
+}
+
+
+// Очистить консоль
+Console.Clear();
+
+double x1 = double.Parse(Console.ReadLine()!);
+double y1 = double.Parse(Console.ReadLine()!);
+double x2 = double.Parse(Console.ReadLine()!);
+double y2 = double.Parse(Console.ReadLine()!);
+
+double distance = GetDistance(x1, y1, x2, y2);
+Console.WriteLine($"{distance:f2}");
