86425901945e19c5037211d397d014010c75a695
Senior Cat <sc0372q@gmail.com>
2022-10-23 20:05:52 +0300

    Lesson 270308. Task 22

---
+++ GB/CSharp/270308/Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 22
+// Напишите задачу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисело от 1 до N
+
+int[] GetScuares(int x)
+{
+    int[] squares = new int[x];
+    for (int i=1; i<=x; i++)
+        squares[i-1] = i * i;
+    return squares;
+}
+
+void PrintSquares(int[] squares)
+{
+    for (int i = 0; i < squares.Length; i++)
+        Console.Write($"{squares[i]} ");
+    Console.WriteLine("");
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine()!);
+int[] squares = GetScuares(x);
+PrintSquares(squares);
