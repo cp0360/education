1093b78db008d925841641d71ebdaecfe126d09c
Senior Cat <sc0372q@gmail.com>
2022-10-23 20:05:03 +0300

    Lesson 270308. Task 17

---
+++ GB/CSharp/270308/Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 17
+// напишите программу, которая принимает на вход координаты точк (X и Y), причем X!=0 и Y!=0
+// и выдает номер номер четверти плоскости, в которой находиться эта точка.
+
+int GetQuater(int x, int y)
+{
+    if (x > 0)
+        if (y > 0)
+            return 1;
+        else
+            return 4;
+    else
+        if (y > 0)
+            return 2;
+        else
+            return 3;
+}
+
+// Очистить консоль
+Console.Clear();
+int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+
+int quater = GetQuater(X, Y);
+Console.WriteLine(quater);
