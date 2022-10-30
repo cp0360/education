4288e9a29af4a5a99d56bf034051fee89ca9a64c
Johnny Catsvill <jc0365@yandex.ru>
2022-10-30 14:18:27 +0300

    Lesson 269083. Task1. Работа с текстом.

---
+++ GB/CSharp/269083/002_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками.
+// Маленькие буквы "к" заменить большими "К"
+// большие "С" заменить на маленькие "с"
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012
+//  s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+
+    return result;
+}
+
+
+Console.Clear();
+text = Replace(text, ' ', '|');
+Console.WriteLine(text);
+
+text = Replace(text, 'к', 'К');
+Console.WriteLine(text);
+
+text = Replace(text, 'С', 'с');
+Console.WriteLine(text);
