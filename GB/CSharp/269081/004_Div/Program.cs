e61498ecaaf9a79cafc1c97f1069bd2359fdbb87
Johnny Catsvill <jc0365@yandex.ru>
2022-10-30 12:30:00 +0300

    Lesson 269081. Task Div. Random

--- GB/CSharp/269081/004_Div/Program.cs
+++ GB/CSharp/269081/004_Div/Program.cs
@@ -1,4 +1,8 @@
-﻿double numberA = 77;
-double numberB = 4;
+﻿double numberA = new Random().Next(10, 100);
+Console.WriteLine($"1-е число: {numberA}");
+
+double numberB = new Random().Next(10, 100);
+Console.WriteLine($"2-е число: {numberB}");
+
 double result = numberA / numberB;
-Console.WriteLine(result);
+Console.WriteLine($"Результат деления {numberA} на {numberB} = {result}");
