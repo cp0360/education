14f3b0709f28fa2feeb9a54ed1b28d4fa2a82016
Johnny Catsvill <jc0365@yandex.ru>
2022-10-30 16:01:57 +0300

    Lesson 269083. Task2. Array sort

---
+++ GB/CSharp/269083/003_Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    int minPosition, temp;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] < array[minPosition])  minPosition = j;
+
+        temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+
+}
+
+
+int[] arr = {16, 4, 7, 21, 2, 0 , 33, 0, 18};
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
