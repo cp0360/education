1c7d28643522efb632aa58acf2a19792c26c346a
Johnny Catsvill <jc0365@yandex.ru>
2022-10-30 19:26:11 +0300

    Lesson 269084. Task3. Recursion Factorial

---
+++ GB/CSharp/269084/003_RecursionFactorial/Program.cs
@@ -0,0 +1,14 @@
+﻿double Factorial (int n)
+{
+    //1! = 1
+    //0! = 1
+    if(n == 1 || n == 0)  return 1;
+    else
+        return n * Factorial(n-1);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+double result = Factorial(n);
+Console.WriteLine($"Факториал {n} = {result}");
