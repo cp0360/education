becb5031b7dbddd1b330220cda47837886600f50
Johnny Catsvill <jc0365@yandex.ru>
2022-10-30 20:13:09 +0300

    Lesson 269084. Task4. Recursion Fibonacci

---
+++ GB/CSharp/269084/004_RecursionFibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else
+        return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+
+Console.Clear();
+for (int i = 1; i < 55; i++)
+{
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
