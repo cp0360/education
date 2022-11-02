5879bde40613df7317c7b92b02491a20a004eedd
Johnny Catsvill <jc0365@yandex.ru>
2022-11-02 21:33:05 +0300

    Lesson 269087. Task2

---
+++ GB/CSharp/269087/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сумма чисел от 1 до n
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 0; i <= n; i++)
+        result += i;
+    return result;
+}
+
+int SumRec(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n + SumRec(n - 1);
+}
+
+Console.WriteLine(SumFor(10));
+Console.WriteLine(SumRec(10));
