ebac7002a80aa9c54c8680ba944e92b629113af7
Johnny Catsvill <jc0365@yandex.ru>
2022-11-02 22:05:26 +0300

    Lesson 269087. Task5

---
+++ GB/CSharp/269087/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Рекурсивный обход директорий
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"/home/jc/Downloads";
+CatalogInfo(path);
