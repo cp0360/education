461cfb16f1e42d507c626f8a630c2cd46a4a686b
Johnny Catsvill <jc0365@yandex.ru>
2022-11-02 21:36:25 +0300

    Lesson 269087. Task3

---
+++ GB/CSharp/269087/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Факториал числа
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else 
+        return n * FactorialFor(n - 1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
