8f957d71efdd3d616912aae8a480e9ea792e5fc4
Johnny Catsvill <jc0365@yandex.ru>
2022-11-08 08:02:39 +0300

    Lesson 270309. Task28

---
+++ GB/CSharp/270309/Task28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение числе от 1 до N
+
+
+int GetProductOfNumbers(int n)
+{
+    int product = 1;
+
+    for (int i = 1; i <= n; i++)
+        product *= i;
+
+    return product;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+int result = GetProductOfNumbers(N);
+Console.WriteLine($"Произведение чисел от 1 до {N} = {result}");
