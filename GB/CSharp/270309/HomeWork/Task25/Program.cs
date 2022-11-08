0380489f46903e74a779db6a10eed65cc35e063d
Johnny Catsvill <jc0365@yandex.ru>
2022-11-08 13:57:49 +0300

    Lesson 270309. HomeWork. Task25

---
+++ GB/CSharp/270309/HomeWork/Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int GetNumberPower(int num, int pow)
+{
+    int res = 1;
+    for (int i = 1; i <= pow; i++)
+        res *= num;
+
+    return res;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите два числа через запятую:");
+
+String[] str = Console.ReadLine()!.Split(",");
+int number = int.Parse(str[0]);
+int pow = int.Parse(str[1]);
+int result = GetNumberPower(number, pow);
+
+Console.WriteLine($"{number} в степени {pow} = {result}");
