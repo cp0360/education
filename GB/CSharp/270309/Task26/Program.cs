fe5042d6147964ff7bb71fdbe39a106277ac40a3
Johnny Catsvill <jc0365@yandex.ru>
2022-11-08 08:02:32 +0300

    Lesson 270309. Task26

---
+++ GB/CSharp/270309/Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите прогрмму, которая принимает на вход число 
+// и выдает кол-во цифр в числе.
+
+
+int GetNumberCount(int X)
+{
+    int count = 0;
+    while (X > 0)
+    {
+        count++;
+        X /= 10;
+    }
+    return count;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine()!);
+int result = GetNumberCount(x);
+Console.WriteLine($"Количество цифр в числе {x} = {result}");
