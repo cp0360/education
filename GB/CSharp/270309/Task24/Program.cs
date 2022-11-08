db9657e1c4882977728749432eee0bb232e7f21a
Johnny Catsvill <jc0365@yandex.ru>
2022-11-08 08:02:25 +0300

    Lesson 270309. Task24

---
+++ GB/CSharp/270309/Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число А
+// и выдаёт сумму чисел от 1 до А
+
+
+int GetSum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i;
+        Console.WriteLine($"{i}: {sum}");
+    }
+    return sum;
+}
+
+Console.Clear();
+int a = int.Parse(Console.ReadLine()!);
+int result = GetSum(a);
+Console.WriteLine(result);
