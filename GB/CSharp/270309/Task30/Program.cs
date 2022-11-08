f1cfe9a6b4d26d8eddbe8b948b41aa1d2ce374dd
Johnny Catsvill <jc0365@yandex.ru>
2022-11-08 08:02:48 +0300

    Lesson 270309. Task30

---
+++ GB/CSharp/270309/Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит массив из 8и элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+
+int[] GetRandomArray()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < 8; i++)
+        arr[i] = new Random().Next(0,2);
+
+    return arr;
+}
+
+int[] result = GetRandomArray();
+
+//Console.Clear();
+Console.Write("[");
+Console.Write(String.Join(", ", result));
+Console.WriteLine("]");
+
+
+// вариант от препода
+//Conosole.Write(String.Join(«,», array))
