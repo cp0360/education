44d3c5f1373b31de8cd17359e824b4c9a83f4225
Johnny Catsvill <jc0365@yandex.ru>
2022-11-09 14:38:55 +0300

    Lesson 270309. HomeWork. Task29

---
+++ GB/CSharp/270309/HomeWork/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void PrintArray()
+{
+    int[] array = new int[8];
+
+    Console.WriteLine("Введите по-очереди 8 чисел.");
+    Console.WriteLine();
+
+    for (int i = 0; i <8; i++)
+    {
+        Console.Write($"Введите {i+1}-e чисело: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    Console.Write("[");
+    Console.Write(String.Join(", ", array));
+    Console.WriteLine("]");
+}
+
+Console.Clear();
+PrintArray();
