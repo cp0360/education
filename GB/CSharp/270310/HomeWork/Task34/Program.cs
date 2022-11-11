fc7cff58731f63bbf1cfefdb95ce7087400b1527
Johnny Catsvill <jc0365@yandex.ru>
2022-11-11 18:15:14 +0300

    Lesson 270310. Homework. Task34

---
+++ GB/CSharp/270310/HomeWork/Task34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int GetNumberEvenElements(int arrLenght)
+{
+    int evenCount = 0;
+    int element;
+    int[] array = new int[arrLenght];
+
+
+    Random rnd = new Random();
+    for (int i = 0; i < arrLenght; i++)
+    {
+        element = rnd.Next(100,1000); 
+        array[i] = element;
+        
+        if (element % 2 == 0) evenCount ++;
+    }
+
+    Console.Write("[");
+    Console.Write(String.Join(", ", array));
+    Console.WriteLine("]");
+
+    return evenCount;
+}
+
+
+Console.Clear();
+Console.WriteLine("Укажите размерность массива:");
+
+int arrLenght = int.Parse(Console.ReadLine()!);
+int result = GetNumberEvenElements(arrLenght);
+
+Console.WriteLine($"В массиве четных элементов: {result}");
