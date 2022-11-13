498b3edd720ddba44ca2f3a6de40c23a03045fcc
Johnny Catsvill <jc0365@yandex.ru>
2022-11-13 16:36:26 +0300

    Lesson 270310. HomeWork. Additional

---
+++ GB/CSharp/270310/HomeWork/AdditionalTask/Program.cs
@@ -0,0 +1,50 @@
+﻿// Разобраться с алгоритмом сортировки методом пузырька. Реализовать невозрастающую сторировку.
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+        res[i] = rnd.Next(minValue, maxValue+1);
+
+    return res;
+}
+
+int[] BoobleSort(int[] arr)
+{
+    int tmp;
+    bool swapped;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < arr.Length - i - 1; j++)
+        {
+            if(arr[j+1] > arr[j])
+            {
+                swapped = true;
+                tmp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+
+        if (!swapped) break; // если небыло перестановок элементов - матсив отсортирован.
+    }
+
+    return arr;
+}
+
+
+
+Console.Clear();
+Console.WriteLine("Укажите размерность массива:");
+
+int arrLenght = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(arrLenght, 10, 99);
+Console.WriteLine(String.Join(" ", array));
+
+int[] reverseSortArray = BoobleSort(array);
+Console.WriteLine(String.Join(" ", reverseSortArray));
