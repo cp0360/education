061b9cb786cbad630f8baa4d0110a4eda31a9a23
Johnny Catsvill <jc0365@yandex.ru>
2022-11-15 22:29:58 +0300

    Lesson 270311. Task45

---
+++ GB/CSharp/270311/Task45/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] CreateArray(int arrlen, int minValue, int maxValue)
+{
+    int[] arr = new int[arrlen];
+    Random rnd = new Random();
+    for (int i = 0; i < arrlen; i++)
+        arr[i] = rnd.Next(minValue, maxValue+1);
+
+    return arr;
+}
+
+int[] CopyArray(int[] arrorig)
+{
+    int[] arrcopy = new int[arrorig.Length];
+    for (int i = 0; i < arrorig.Length; i++)
+        arrcopy[i] = arrorig[i];
+
+    return arrcopy;
+}
+
+
+Console.WriteLine("Укажите размерность массива:");
+int arrLen = int.Parse(Console.ReadLine()!);
+int[] origArray = CreateArray(arrLen, 10,99);
+
+Console.Write("Original array: ");
+Console.WriteLine(string.Join(" ", origArray));
+
+int[] copyArray = CopyArray(origArray);
+Console.Write("Copy array: ");
+Console.WriteLine(string.Join(" ", copyArray));
