56bf128bfe4ef541b04885a3da65bd159d32eb15
Johnny Catsvill <jc0365@yandex.ru>
2022-11-15 22:56:23 +0300

    Lesson 270311. Task44

---
+++ GB/CSharp/270311/Task44/Program.cs
@@ -0,0 +1,26 @@
+﻿// Не исопльзуя рекурсию, выведите превые N чисел Фиббоначи.
+// Первые два числа Фиббоначи: 0 и 1
+
+
+
+int[] GetFibArray(int arrLen)
+{
+    int[] fibArray = new int[arrLen];
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        if (i == 0 || i == 1) 
+            fibArray[i] = i;
+        else
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+
+    return fibArray;
+}
+
+
+Console.WriteLine("Укажите длину последовательности Фиббоначи:");
+int N = int.Parse(Console.ReadLine()!);
+int[] array = GetFibArray(N);
+
+Console.WriteLine(string.Join(" ", array));
