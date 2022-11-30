8283ed48ff2d3660b98e4fb47ff3aa3de5d2c08e
Johnny Catsvill <jc0365@yandex.ru>
2022-11-30 13:49:13 +0300

    Lesson 270314. HomeWork. Task66

---
+++ GB/CSharp/270314/HomeWork/Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+int CalculateSum(int M, int N)
+{
+    int total = 0;
+    for (int i = M; i <= N; i++)
+        total += i;
+
+    return total;
+}
+
+
+Console.Clear();
+Console.Write("Укажите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Укажите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n < m)
+{
+    System.Console.WriteLine($"N не може быть меньше M");
+    return;
+}
+
+int total = CalculateSum(m,n);
+System.Console.WriteLine($"M = {m}; N = {n} -> {total}");
