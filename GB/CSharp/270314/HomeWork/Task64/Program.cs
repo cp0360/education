769b07aed320c343ede3ebc902fd19973adf3a72
Johnny Catsvill <jc0365@yandex.ru>
2022-11-30 13:39:40 +0300

    Lesson 270314. HomeWork. Task64

---
+++ GB/CSharp/270314/HomeWork/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void PrintSequence(int n)
+{
+    if(n==1)
+    {
+        Console.Write(n);
+        return;
+    }
+
+    Console.Write($"{n}, ");
+    PrintSequence(n-1);
+}
+
+Console.Clear();
+Console.Write("Укажите длину последовательности: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"N = {n} -> \"");
+PrintSequence(n);
+Console.WriteLine("\"");
