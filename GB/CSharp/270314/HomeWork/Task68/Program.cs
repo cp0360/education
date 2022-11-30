8e5403e33f0dc6a3e99b26546ccb747f4b63ff2e
Johnny Catsvill <jc0365@yandex.ru>
2022-11-30 15:22:39 +0300

    Lesson 270314. HomeWork. Task68

---
+++ GB/CSharp/270314/HomeWork/Task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+// функция Аккермана
+int FAkkerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return FAkkerman(n - 1, 1);
+    else
+      return FAkkerman(n - 1, FAkkerman(n, m - 1));
+}
+
+
+Console.Clear();
+Console.Write("Укажите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Укажите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+
+int result = FAkkerman(m, n);
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {result}");
